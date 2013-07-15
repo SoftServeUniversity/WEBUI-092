0b05d6970d07225aa67f7ead641a8045f3f235cb
olena <lena.ohuryayeva@gmail.com>
2013-07-15 14:54:44 +0300

    some changes

--- app/collections/departments/CoursesCollection.js
+++ app/collections/departments/CoursesCollection.js
@@ -5,8 +5,10 @@ define([
     'models/department/CourseModel'
 ], function($, _, Backbone, CourseModel){
 
-        return  Backbone.Collection.extend({
-            model:CourseModel
-        });
+    var CoursesCollection = Backbone.Collection.extend({
+        model:CourseModel
+    });
+
+    return CoursesCollection;
 
 });
