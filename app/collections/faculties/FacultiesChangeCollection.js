3e8ecc80d964b6680ebf86b20861bc444b488dfc
yura <yuriy.zdyrko@gmail.com>
2013-07-19 01:51:44 +0300

    moved collections to json files and made small changes to the look of front page

--- app/collections/faculties/FacultiesChangeCollection.js
+++ app/collections/faculties/FacultiesChangeCollection.js
@@ -4,89 +4,9 @@ define([
   'backbone',
 ], function($, _, Backbone){
 	
-  var Test = [
-	{
-		"id":1,
-	    "progress":"0.9",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":2,
-	    "progress":"0.6",
-	    "created_at":"19 08 2011",
-	    "updated_at": "22 08 2011"
-	
-	},
-	{
-		"id":3,
-	    "progress":"0.3",
-	    "created_at":"19 08 2010",
-	    "updated_at": "22 08 2010"
-	
-	},
-	{
-		"id":4,
-	    "progress":"0.1",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":5,
-	    "progress":"0.5",
-	    "created_at":"19 08 2010",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":6,
-	    "progress":"0.3",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":7,
-	    "progress":"0.7",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":8,
-	    "progress":"0.5",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":9,
-	    "progress":"0.6",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":10,
-	    "progress":"0.4",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	},
-	{
-		"id":11,
-	    "progress":"0.6",
-	    "created_at":"19 08 2013",
-	    "updated_at": "22 08 2013"
-	
-	}
-];
-	
 
   var FacultiesChangeCollection = function(){
-  	return new Backbone.Collection(Test);
+  	return new Backbone.Collection();
   }
   return FacultiesChangeCollection;
 });
--- app/views/faculty/FacultiesListView.js
+++ app/views/faculty/FacultiesListView.js
@@ -4,7 +4,8 @@ define([
   'backbone',
   'text!templates/faculty/facultiesListTemplate.html',
   'collections/faculties/FacultiesCollection',
-  'collections/faculties/FacultiesChangeCollection'
+  'collections/faculties/FacultiesChangeCollection',
+  'libs/jquery/equal-height-blocks'
 
 ], function($, _, Backbone, facultiesListTemplate, FacultiesCollection, FacultiesChangeCollection){
 
@@ -23,14 +24,21 @@ define([
       $('.menuBox li a[href="#"]').parent().addClass('active');
       
 
-
-
-
 		var facs_col = new FacultiesCollection();
+		facs_col.fetch({ url: "app/collections/faculties/facultiesCollection.json", async:false, success: function() {
+		    console.log(facs_col);    
+		}});
+		
 		var changes_col = new FacultiesChangeCollection();
+		changes_col.fetch({ url: "app/collections/faculties/facultiesChangeCollection.json", async:false, success: function() {
+		    console.log(changes_col);  
+		}});		
+		
+		
 		var facs = facs_col.toJSON();
 		var changes = changes_col.toJSON();
-
+		
+		
 		var facs_changes = _.map(facs, function (fac) {  
 		    var change = _.find(changes, function (o) { 
 		        return o.id == fac.id;
