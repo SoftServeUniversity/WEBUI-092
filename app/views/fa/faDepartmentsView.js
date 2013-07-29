2201b45b0d4b5a496ac9c940ef6fbe7007fd437a
ozaretc <olegzaremba@gmail.com>
2013-07-29 14:52:27 +0300

    Added regex validation and alerts

--- app/views/fa/faDepartmentsView.js
+++ app/views/fa/faDepartmentsView.js
@@ -8,7 +8,7 @@ define([
   //для виведення селектів завантажуєм колекцію факультетів
   'collections/faculties/FacultiesCollection',
 
-], function($, _, Backbone, departmentsListTemplate, FaDepartmentsCollection, FacultiesCollection){   
+], function($, _, Backbone, departmentsListTemplate, FaDepartmentsCollection, FacultiesCollection, FaRoleModel){   
 
 
   var FaDepartmentsView = Backbone.View.extend({
@@ -88,8 +88,17 @@ define([
        $(e.target).css('display', 'none').next().css('display','block');	 
     },
     saveData: function (e){
-    	//тут я ще нічого не зробив
-      alert('Data Saved!');
+      var name = document.getElementById("dept-name").value;
+      var ck_name = /^[A-Za-z0-9 ]{3,20}$/;
+      if (ck_name.test(name)) {
+        $('#department-header').append("<div class='alert alert-success'><strong>Success!</strong>You have successfully created a department.</div>");
+        window.setTimeout(function () {
+          $('.alert-success').fadeOut();
+        }, 2000);
+      }
+      else{
+        $('#department-header').append("<div class='alert alert-error'><a class='close' data-dismiss='alert'>×</a> <strong>Error!</strong>Name should be between 3 and 20 characters.</div>");
+      }
     },
     openModal: function(){
       $('#menage-department').modal('show');
@@ -102,7 +111,7 @@ define([
       $('#menage-department-import').modal('show');
     },
     newDepartment: function(){
-      $('#content-table').append("<tr><td class='text-center'><input type='text' size='10' placeholder='Ender Name'></td><td class='text-center'><select><option selected value='Крокодил Гена'>Крокодил Гена</option><option value='Edvart Шапокляк'>Edvart Шапокляк</option><option value='Лариса Linkoln'>Лариса Linkoln</option></select></td><td class='text-center'><select><option selected value='Doing Nothing'>Doing Nothing</option><option value='Pickup'>Pickup</option><option value='Mathematics'>Mathematics</option></select></td><td class='text-center'><button class='btn btn-success' id='create_button'>Create</button></td></tr>");
+      $('#content-table').append("<tr><td class='text-center'><input id='dept-name' type='text' size='10' placeholder='Enter Name'/></td><td class='text-center'><select><option selected value='Крокодил Гена'>Крокодил Гена</option><option value='Edvart Шапокляк'>Edvart Шапокляк</option><option value='Лариса Linkoln'>Лариса Linkoln</option></select></td><td class='text-center'><select><option selected value='Doing Nothing'>Doing Nothing</option><option value='Pickup'>Pickup</option><option value='Mathematics'>Mathematics</option></select></td><td class='text-center'><button class='btn btn-success' id='create_button'>Create</button></td></tr>");
     }
   });
   return  FaDepartmentsView;
