bf6254e6b509d5e369217392ab2cbc7a105bd283
jonfen <jonfen@jonfen-PC.(none)>
2013-07-30 23:56:04 +0300

    deleted history view

--- app/views/work/TasksListView.js
+++ app/views/work/TasksListView.js
@@ -1,28 +1,28 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'views/work/ElementView'
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/work/ElementView'
 ], function($, _, Backbone, ElementView){
 
-    var ListView = Backbone.View.extend({
-        collection : null,
-        tagName: 'div',
-        initialize: function (){
-        },
-        render:function(){
-            // counter -  is used for mark each element in a table
-            // linkTo - is used for creating <a> links if it is needed
-                var counter = 1;
-                this.collection.each(function(element) {
-                    var elementView = new ElementView({model: element, linkTo: this.options.linkTo});
-                    this.$el.append(elementView.render(counter++).el);
-                }, this);
+  var ListView = Backbone.View.extend({
+    collection : null,
+    tagName: 'div',
+    initialize: function (){
+    },
+    render:function(){
+      // counter -  is used for mark each element in a table
+      // linkTo - is used for creating <a> links if it is needed
+        var counter = 1;
+        this.collection.each(function(element) {
+          var elementView = new ElementView({model: element, linkTo: this.options.linkTo});
+          this.$el.append(elementView.render(counter++).el);
+        }, this);
 
-            return this;
-        }
+      return this;
+    }
 
-    });
-    return  ListView;
+  });
+  return  ListView;
 });
 
