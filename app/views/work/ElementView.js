643c8415a7a934f0787d2bd818674e2d08d9a414
jonfen <jonfen@jonfen-PC.(none)>
2013-08-01 23:07:12 +0300

    pull jasmine

--- app/views/work/ElementView.js
+++ app/views/work/ElementView.js
@@ -16,7 +16,7 @@ define([
     render:function(counter){
       // counter -  is used for mark each element in a table
       // linkTo - is used for creating <a> links if it is needed
-      this.model.set('counter', counter);
+
       this.model.set('linkTo', this.options.linkTo);
       this.$el.html(this.template(this.model.toJSON()));
       return this;
