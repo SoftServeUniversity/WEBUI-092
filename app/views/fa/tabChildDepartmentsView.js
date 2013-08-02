2ce49574ffa594c8048909e90aa78830e5b01734
yura <yuriy.zdyrko@gmail.com>
2013-08-02 16:43:19 +0300

    inputs in admin tables  contain good data

--- app/views/fa/tabChildDepartmentsView.js
+++ app/views/fa/tabChildDepartmentsView.js
@@ -35,13 +35,13 @@ define([
             _link: 'teacher_id',
             label: 'Department Head',
             type:'select',
-            src:this.teachers_col.models
+            src:this.teachers_col.toJSON()
           },
           {
             _link: 'faculty_id',
             label: 'Faculty Name',
             type:'select',
-            src:this.faculties_col.models
+            src:this.faculties_col.toJSON()
           }
         ]
       };
@@ -58,7 +58,7 @@ define([
       this.teachers_col = new TeachersCollection();
       
       $.when(this.departments_col.fetch() && this.faculties_col.fetch() && this.teachers_col.fetch()).then(function(){
-        that.trigger('onDataLoaded')
+        that.trigger('onDataLoaded');
       })
     },
 
@@ -71,15 +71,77 @@ define([
       that.loadData(); 
        
       this.on('onDataLoaded', function(){
-      config = that.setConfig();
-      this.render(config);
-      console.log(config)
+      	config = that.setConfig();
+      
+      	this.buildJSON(config);
+
+      
+      
+      
       //trigger global event (we listen to it in tabParentView.js)
       vent.trigger('tabChildSupViewLoaded');
       }); 
            
     },
     
+    buildJSON: function(config){
+    	//loop through all entities
+    		var rel = {};
+    		var visible_fields = [];
+    	    //loop through data
+    	    for (i=0; i<config.data.length; i++) {
+    	    	
+    	    	for (var d_field in config.data[i]){
+    	    	
+    	    	}
+    	    	
+    	    	if (config.data[i]['src']){
+    	    		 //console.log(config.data[i]['src'])
+    	    		 var rel_link = config.data[i]['_link'];
+    	    		 
+    	    		 var rel_src = config.data[i]['src'];
+    	    		 
+    	    		 var obj = {};
+    	    		 
+    	    		 obj = rel_src;
+    	    		 
+    	    		 //array of foreign keys, mapped to collections
+    	    		 rel[rel_link]=obj;
+    	    		 
+				}
+				visible_fields.push(config.data[i]['_link'])
+				
+				
+
+    	    }
+
+    	    for (a=0; a<config.entity.length; a++){
+    	    	 config.entity[a]['selectbox_items'] = []	
+
+    	    	for (var e_obj in config.entity[a]){
+    	    		
+    	    		if (e_obj in rel){
+    
+    	    		  //console.log(rel[e_obj]);
+    	    		  config.entity[a]['selectbox_items'].push(e_obj);
+    	    		  config.entity[a][e_obj+'_collection'] = rel[e_obj];
+    	    		  config.entity[a]['visible_fields'] = visible_fields;
+    	    		}
+    
+    	    	}
+
+    	    }
+    	 data = {
+    	 	
+    	 	entities: config.entity
+    	 };
+    	 //console.log(data)
+     	this.render(data);
+    	
+    	
+    	
+    	
+    },
     
     events: {
     },
