012df3076c1b94c198234b88db760aadf1ead71e
yurko <yurko.krain@gmail.com>
2013-08-06 22:22:34 +0300

    requirement fix

--- public/app/models/student/StudentProxyModel.js
+++ public/app/models/student/StudentProxyModel.js
@@ -7,7 +7,6 @@ define([
   var StudentProxyModel = Backbone.Model.extend({
     initialize : function (attributes) {
         var student = new StudentModel( {id: attributes.id});
-        console.log(this);
         this.set('name', attributes.name +' ' +attributes.patronymic + ' ' + attributes.surname);
         this.set('progress', attributes.progress) ;
         this.set('name_with_url', '<a href=#/work/'+this.id+'>'+this.get('name')+'</a>');
