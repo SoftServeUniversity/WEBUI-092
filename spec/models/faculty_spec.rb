887e63ea764d7fc26cd1d3246a356c02e043b7e7
olena <lena.ohuryayeva@gmail.com>
2013-08-11 23:18:50 +0300

    add spec test for Faculty model

--- spec/models/faculty_spec.rb
+++ spec/models/faculty_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Faculty do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @faculty = Faculty.new name: 'Faculty name' }
+  subject { @faculty }
+
+  it { should respond_to(:name)}
+  it { should be_valid}
+  it "name should not be more than 45 symbols" do
+    unvalid_post = Faculty.new name: ("*"*46)
+    unvalid_post.should_not be_valid
+  end
+
+
 end
