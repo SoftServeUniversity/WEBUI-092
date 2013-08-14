b7574fa38b00e23230ece8facba18a3c1a0dbc44
banzay <yuriy.zdyrko@gmail.com>
2013-08-14 11:41:16 +0300

    changed collection loadin in admin area to callbacks

--- public/app/tests/spec/admin/ParentAdminViewSpec.js
+++ public/app/tests/spec/admin/ParentAdminViewSpec.js
@@ -1,26 +1,3 @@
-/*describe 'NewCardView', ->
-
-  view = null
-  beforeEach ->
-    loadFixtures "index.html"
-    view = new NewCardView
-
-  it 'binds $el to the right DOM element', ->
-    expect( view.$el ).toBe( 'section#new-card' )
-
-  it 'triggers a create-card event when the add card button is clicked', ->
-    eventSpy = sinon.spy()
-    view.on( 'create-card', eventSpy )
-
-    $('section#new-card button').click()
-
-    expect( eventSpy ).toHaveBeenCalledWith(view)
-
-
-
-.delete-button
-*/
-
 describe('Parent Admin View :: event handlers on user actions', function() {
   var adminView;
   beforeEach(function(){
@@ -38,8 +15,8 @@ describe('Parent Admin View :: event handlers on user actions', function() {
 
   });
   
-  //цей виконається (teacher має метод sayBuhaha, що повертає 'bu-ha-ha')
-  it('should be truthy', function(){
+
+   it('should be truthy', function(){
      expect(typeof adminView.initialize).toBe("function")  
    });
   
