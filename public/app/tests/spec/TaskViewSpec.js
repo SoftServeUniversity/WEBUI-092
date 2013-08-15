2e2f0c18d8a705e5c2ade67535656bdcb07837c0
Sergey Karelov <sergey.karelov@gmail.com>
2013-08-15 14:41:53 +0300

    tests with jasmine

---
+++ public/app/tests/spec/TaskViewSpec.js
@@ -0,0 +1,25 @@
+describe("Task View", function() {
+	var taskView;
+  beforeEach(function(){
+	var me = this;
+    GlobalEventBus = _.extend({}, Backbone.Events);
+    runs( function () {
+        require(['views/task/taskView'], function(TaskView) {
+          taskView = new TaskView();
+        })
+    });
+
+    waitsFor(function () { return taskView; } , 'Admin view load timeout', 1000);
+	});
+
+  it("should have an initialize method", function() {
+    expect(typeof taskView.initialize).toBe("function");
+  });
+  it("should have succeed initialize", function() {
+    expect(taskView.initialize()).toBeTruthy();
+  });
+  it("should have succeed render", function() {
+    expect(taskView.render()).toBeTruthy();
+  });
+
+});
