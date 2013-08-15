b1f661ddb4a88427fb290dae321e31fcb12977c3
q <q@q-M11x.(none)>
2013-08-15 14:37:51 +0300

    beckend department validation

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,9 +3,10 @@ class Department < ActiveRecord::Base
 
   #has_one :head, class_name: 'User', foreign_key: :department_id
   
-  validates :faculty_id, length: { in: 1...45 }
-  validates :name, 		 length: { in: 1...45 }
-  validates :user_id, 	 length: { in: 1...45 }
+  validates :name, 		 length: { in: 1...256 }
+  validates :faculty_id, presence: true
+  NAMEREGEX = /[\w\s'",.а-яіїґ-—]{1,256}i/
+  validates :name, format: {whith: NAMEREGEX}
 
   belongs_to :faculty
 end
