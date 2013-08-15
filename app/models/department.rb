fdac4c3a65210e196ada9e01af576758c5228f83
q <q@q-M11x.(none)>
2013-08-15 15:26:14 +0300

    solves problem with reg exp

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,11 +2,9 @@ class Department < ActiveRecord::Base
   attr_accessible :faculty_id, :name, :user_id
 
   #has_one :head, class_name: 'User', foreign_key: :department_id
-  
-  validates :name, 		 length: { in: 1...256 }
+  validates :name, length: { in: 1...256 }
   validates :faculty_id, presence: true
-  NAMEREGEX = /[\w\s'",.а-яіїґ-—]{1,256}i/
-  validates :name, format: {whith: NAMEREGEX}
+  #validates :name, format: { whith: /[\w\s'",.а-яіїґ-—]{1,256}/i, message: 'some message' }
 
   belongs_to :faculty
 end
