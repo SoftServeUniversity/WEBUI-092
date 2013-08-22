e83286c63d80cdfe99b57fe84cf95cc6bba024f2
banzay <yuriy.zdyrko@gmail.com>
2013-08-22 22:13:30 +0300

    added Works tab to Faculty admin

--- public/app/views/admin/adminFacultyView.js
+++ public/app/views/admin/adminFacultyView.js
@@ -13,11 +13,12 @@ define([
   
   'views/admin/tabGroupsView',
   'views/admin/tabDepartmentsView',
-  'views/admin/tabTeachersView'
-
+  'views/admin/tabTeachersView',
+  'views/admin/tabWorksView'
 
 ], function ($, bootstrapselect, _,  Backbone,
-            ParentAdminView, TabAdminsView, TabCoursesView, TabGroupsView, TabDepartmentsView, TabTeachersView) {   
+            ParentAdminView, TabAdminsView, TabCoursesView,
+             TabGroupsView, TabDepartmentsView, TabTeachersView, TabWorksView) {   
   
 var AdminFacultyView = ParentAdminView.extend({  
   
@@ -47,6 +48,11 @@ var AdminFacultyView = ParentAdminView.extend({
       id:'groups-tab',
       label: 'Групи',
       action: 'manage_groups'
+    },
+    {
+      id:'works-tab',
+      label: 'Роботи',
+      action: 'manage_works'
     }
   ],
   
@@ -85,6 +91,11 @@ var AdminFacultyView = ParentAdminView.extend({
   manage_teachers: function(){
     this.activeMenuId = 'teachers-tab';
     this.tabView = new TabTeachersView();
+    this.showAdminButtons();
+  },
+  manage_works: function(){
+    this.activeMenuId = 'works-tab';
+    this.tabView = new TabWorksView();
     this.showAdminButtons();
   }
 
