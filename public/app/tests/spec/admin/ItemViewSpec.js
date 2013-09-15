2a90b400a2b647b41b2fa1b96572c4be639bf6af
banzay <yuriy.zdyrko@gmail.com>
2013-09-15 15:38:14 +0300

    merged more changes

--- public/app/tests/spec/admin/ItemViewSpec.js
+++ public/app/tests/spec/admin/ItemViewSpec.js
@@ -1,18 +1,19 @@
 describe('ItemView', function() {
 
     var me = this; 
-    require(['models/course/CourseModel', 'views/admin/itemView'], function(CourseModel, ItemView) {
-      me.model = new CourseModel({
+    require(['models/course/CourseModel'], function(CourseModel) {
+      /*me.model = new CourseModel({
         id: 1,
         name: "Course_ABD0",
         year_of_start: "2012-02-01",
         faculty_id: 1,
         created_at: "2013-09-11 06:09:18",
         updated_at: "2013-09-11 06:09:18"
-      })
+      })*/
+      //console.log(CourseModel)
     
-      me.view = ItemView;
-          console.log('me.view')
+      /*me.view = ItemView;
+          console.log('me.view')*/
 
     })
 
@@ -36,7 +37,7 @@ describe('ItemView', function() {
 
 
     //$('body').append('<table id="itemsTable"></table>');
-    this.ItemView = new me.view({ model: me.model, conf: me.config });
+    //this.ItemView = new me.view({ model: me.model, conf: me.config });
   
 
 
