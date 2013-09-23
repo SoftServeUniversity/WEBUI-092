ca94d0cc314fb456655923e529e7e919e3e8c163
YurkoHoshko <yurko.krain@gmail.com>
2013-09-23 13:08:52 +0300

    Mysql backup logic + scheduled backups

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -1,22 +1,23 @@
 #encoding: utf-8
 class BackupController < ApplicationController
+  require 'yaml'
+  respond_to :json
 
 
   def full_backup
-    path = Rails.root.join('backup',Time.now.strftime("%Y-%d-%m_%H:%M")+'-full_backup').to_s
-    `pg_dump webui_dev > '#{path}'`
-
-    send_file(path, status: 200)
+    @path = Rails.root.join('backup',Time.now.strftime("%Y-%d-%m_%H:%M:%S")+'-full_backup.sql').to_s
+    @config = YAML.load_file(Rails.root.join('config','backup_config.yml').to_s)
+    `mysqldump -u #{@config['username']}  -p#{@config['password']} #{@config['database']} > #{@path}`
+    send_file(@path, status: 200)
   end
 
-  def restore_from_backup(infile)
-    pid = File.read(Rails.root.join('tmp','pids','server.pid').to_s)
-    puts 'ok'
-    `kill -9 #{pid}`
-    puts 'ok1'
-    `dropdb webui_dev | create_db webui_dev`
-    `psql webui_dev < #{infile}`
-    `rails s`
+  private
 
+  def self.regular_backup
+    @path = Rails.root.join('backup',Time.now.strftime("%Y-%d-%m_%H:%M:%S")+'-regular_backup.sql').to_s
+    @config = YAML.load_file(Rails.root.join('config','backup_config.yml').to_s)
+    `mysqldump -u #{@config['username']}  -p#{@config['password']} #{@config['database']} > #{@path}`
   end
+
+
 end
